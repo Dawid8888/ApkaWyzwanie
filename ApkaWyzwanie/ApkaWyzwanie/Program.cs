5c2c8de6542ade61a2dff44dbdca603e30779ac7
Dawid8888 <dawidszczyglo@gmail.com>
2023-01-31 22:05:24 +0100

    dalsza nauka

--- ApkaWyzwanie/ApkaWyzwanie/Program.cs
+++ ApkaWyzwanie/ApkaWyzwanie/Program.cs
@@ -1,2 +1,38 @@
-﻿Console.WriteLine("Hello, Dawid!");
-Console.WriteLine("latam sobie");
+﻿// Start
+//Console.WriteLine("Hello Dawid");
+
+// zmienne ang.variables
+// liczby całkowite
+int myAge = 120;
+int myAge1 = 100;
+int myAge2 = 180;
+int wynikMyAges = myAge+myAge1+myAge2+5+15;
+//Console.WriteLine(wynikMyAges);
+int myInt = int.MaxValue;
+uint myInt2 = uint.MaxValue;
+long myInt3 = long.MaxValue;
+ulong myInt4 = ulong.MaxValue;
+decimal myInt5 = decimal.MaxValue;
+
+// liczby zmiennoprzecinkowe
+float myNumber = float.MaxValue;
+double myNumber2 = double.MaxValue;
+
+// zmienne tekstowe
+string name = "Dawid";
+string name2 = "Aleksander";
+string surname = "Szczygło";
+string results = name+name2+surname+wynikMyAges;
+//Console.WriteLine(results);
+char mała = 'c';
+var result2 = name.ToArray();
+
+// zmienna logiczna bool - okresla dwie wartosci true or false
+bool isVali = 5 < 6;
+var iscos = 2 > 2;
+var iscostam = 2 + 3 + 4;
+Console.WriteLine(isVali);
+Console.WriteLine(iscos);
+Console.WriteLine(iscostam);
+var nejmy = name + surname;
+Console.WriteLine(nejmy);
